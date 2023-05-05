a0e56b98f00d08e9b9cce4f6dd2b015a7a2b4fdd
Ricardo Wurmus <rekado@elephly.net>
2023-05-05 21:31:26 +0200

    bioinformatics: Add meme.
    * guix-science-nonfree/packages/bioinformatics.scm (meme): New variable.

--- guix-science-nonfree/packages/bioinformatics.scm
+++ guix-science-nonfree/packages/bioinformatics.scm
@@ -19,6 +19,7 @@
   #:use-module (guix-science-nonfree licenses)
   #:use-module (guix-science-nonfree packages cuda)
   #:use-module (gnu packages)
+  #:use-module (gnu packages admin)
   #:use-module (gnu packages algebra)
   #:use-module (gnu packages base)
   #:use-module (gnu packages bash)
@@ -31,10 +32,12 @@
   #:use-module (gnu packages cran)
   #:use-module (gnu packages documentation)
   #:use-module (gnu packages gcc)
+  #:use-module (gnu packages ghostscript)
   #:use-module (gnu packages image)
   #:use-module (gnu packages java)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages maths)
+  #:use-module (gnu packages mpi)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
@@ -42,6 +45,7 @@
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages statistics)
   #:use-module (gnu packages swig)
+  #:use-module (gnu packages web)
   #:use-module (gnu packages xml)
   #:use-module (past packages boost)
   #:use-module (past packages perl)
@@ -397,6 +401,128 @@ tool."))))
 factor binding sites among upstream sequences from co-regulated
 genes.")
     (license (nonfree "Academic use only."))))
+
+(define-public meme
+  (package
+    (name "meme")
+    (version "4.11.3_1")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "http://meme-suite.org/meme-software/"
+                                  (car (string-split version #\_))
+                                  "/meme_" version ".tar.gz"))
+              (sha256
+               (base32
+                "08df4wgiz1baq3749slpmr7df0hg3q4i3cdvap97xw063kx2d9gc"))))
+    (build-system gnu-build-system)
+    (arguments
+     (list
+      ;#:make-flags '(list "CFLAGS=-O2 -g -fcommon")
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'fix-paths-to-tools
+            (lambda _
+              (substitute* "src/utils.c"
+                (("\"hostname")
+                 (string-append "\"" (which "hostname"))))))
+          (add-after 'unpack 'remove-unused-tests
+            (lambda _
+              ;; We don't build the web server stuff, so we don't need
+              ;; to run the tests for that either.
+              (substitute* "tests/scripts/Makefile.in"
+                (("tomtom.test") ""))))
+          (add-before 'configure 'check-perl-dependencies
+            (lambda _
+              (invoke "perl" "./scripts/dependencies.pl")))
+          (add-after 'install 'wrap-perl-scripts
+            (lambda* (#:key inputs outputs #:allow-other-keys)
+              ;; Make sure perl scripts finds all perl inputs at runtime.
+              (for-each (lambda (prog)
+                          (wrap-program (string-append #$output "/bin/" prog)
+                            `("PERL5LIB" ":" prefix
+                              (,(getenv "PERL5LIB")))))
+                        '("ama-qvalues"
+                          "beeml2meme"
+                          "chen2meme"
+                          "dreme_xml_to_html"
+                          "dreme_xml_to_txt"
+                          "elm2meme"
+                          "fasta-center"
+                          "fasta-fetch"
+                          "fasta-grep"
+                          "fasta-make-index"
+                          "fasta-most"
+                          "fasta-subsample"
+                          "fasta-unique-names"
+                          "hart2meme-bkg"
+                          "hartemink2psp"
+                          "iupac2meme"
+                          "jaspar2meme"
+                          "mast_xml_to_html"
+                          "mast_xml_to_txt"
+                          "matrix2meme"
+                          "meme-chip"
+                          "meme-rename"
+                          "meme_xml_to_html"
+                          "nmica2meme"
+                          "priority2meme"
+                          "psp-gen"
+                          "rna2meme"
+                          "rsat-retrieve-seq"
+                          "rsat-supported-organisms"
+                          "scpd2meme"
+                          "sites2meme"
+                          "taipale2meme"
+                          "tamo2meme"
+                          "tomtom_xml_to_html"
+                          "transfac2meme"
+                          "uniprobe2meme")))))))
+    (native-inputs (list gcc-8))
+    (inputs
+     (list perl
+           perl-file-which
+           perl-html-parser
+           perl-html-template
+           perl-xml-simple
+           perl-xml-compile
+           perl-xml-compile-wsdl11
+           perl-xml-parser
+           python-2                     ;only works with Python 2
+           libxml2
+           libxslt
+           openmpi
+           ghostscript
+           inetutils                    ;for "hostname"
+           zlib))
+    (propagated-inputs
+     ;; "which" must be propagated because of the weird way it is used
+     ;; in "src/exec_parallel.c".  The buffer "cmd_len" is arranged to
+     ;; be 6 characters longer than the argument, just enough for the
+     ;; string "which ".  I don't want to mess with pointers and
+     ;; buffer lengths just to hardcode a path to the "which"
+     ;; executable.
+     (list which))
+    (home-page "http://www.tbi.univie.ac.at/RNA/index.html")
+    (synopsis "Motif-based sequence analysis tools")
+    (description
+     "The MEME Suite allows the biologist to discover novel motifs in
+collections of unaligned nucleotide or protein sequences, and to
+perform a wide variety of other motif-based analyses.
+
+The MEME Suite supports motif-based analysis of DNA, RNA and protein
+sequences.  It provides motif discovery algorithms using both
+probabilistic and discrete models, which have complementary strengths.
+It also allows discovery of motifs with arbitrary insertions and
+deletions (GLAM2).  In addition to motif discovery, the MEME Suite
+provides tools for scanning sequences for matches to motifs (FIMO,
+MAST and GLAM2Scan), scanning for clusters of motifs (MCAST),
+comparing motifs to known motifs (Tomtom), finding preferred spacings
+between motifs (SpaMo), predicting the biological roles of
+motifs (GOMo), measuring the positional enrichment of sequences for
+known motifs (CentriMo), and analyzing ChIP-seq and other large
+datasets (MEME-ChIP).")
+    (license (nonfree "http://meme-suite.org/doc/copyright.html"
+                      "license forbids commercial usage"))))
 
 (define-public rmats-turbo
   (package
