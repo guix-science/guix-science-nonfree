4f425c370240bf41a1531ebc6b88a3d7ddf1dc4c
Ricardo Wurmus <rekado@elephly.net>
2024-01-25 10:25:45 +0100

    licenses: Add artistic-1.0.
    * guix-science-nonfree/licenses.scm (artistic-1.0): New variable.

--- guix-science-nonfree/licenses.scm
+++ guix-science-nonfree/licenses.scm
@@ -1,6 +1,7 @@
 ;;;
 ;;; Copyright © 2019 Alex Griffin <a@ajgrf.com>
 ;;; Copyright © 2021 Lars-Dominik Braun <ldb@leibniz-psychology.org>
+;;; Copyright © 2024 Ricardo Wurmus <rekado@elephly.net>
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -16,7 +17,7 @@
 ;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (define-module (guix-science-nonfree licenses)
-  #:export (nonfree cc-by-nd4.0))
+  #:export (nonfree artistic-1.0 cc-by-nd4.0))
 
 ;; Guix does not export the license record constructor.
 (define license (@@ (guix licenses) license))
@@ -29,6 +30,11 @@ at URI, which may be a file:// URI pointing the package's tree."
            (string-append
             "This a nonfree license.  Check the URI for details.  "
             comment)))
+
+(define artistic-1.0
+  (license "Artistic 1.0"
+           "https://opensource.org/license/artistic-1-0/"
+           "Artistic License 1.0"))
 
 (define cc-by-nd4.0
   (license "CC-BY-ND 4.0"
