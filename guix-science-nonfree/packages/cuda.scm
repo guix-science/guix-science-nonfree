fac9ca892e969c8ddfc3173b3d2f1768ecab58e2
Atte Torri <atte@torri.be>
2024-03-21 18:52:58 +0100

    cuda: Add cutensor.
    * guix-science-nonfree/packages/cuda.scm (cutensor): New variable.
    
    Signed-off-by: Ricardo Wurmus <rekado@elephly.net>

--- guix-science-nonfree/packages/cuda.scm
+++ guix-science-nonfree/packages/cuda.scm
@@ -1,5 +1,6 @@
 ;;; Copyright © 2018, 2019, 2020 Inria
 ;;; Copyright © 2021-2024 Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
+;;; Copyright © 2024 Atte Torri <atte.torri@protonmail.com>
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify it
 ;;; under the terms of the GNU General Public License as published by
@@ -17,6 +18,7 @@
 (define-module (guix-science-nonfree packages cuda)
   #:use-module (guix)
   #:use-module (guix gexp)
+  #:use-module (guix build-system copy)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system trivial)
   #:use-module ((guix licenses) #:prefix license:)
@@ -425,3 +427,29 @@ See also
                  (string-append "https://developer.download.nvidia.com/compute/cudnn/redist/cudnn/linux-x86_64/cudnn-linux-x86_64-"
                                 version "_cuda11-archive.tar.xz")
                  "0p286gnjslz06z9vff136pq8srkax75nbklmvg4r11g2cxr8ind6"))))
+
+(define-public cutensor
+  (package
+    (name "cutensor")
+    (version "2.0.1.2")
+    (home-page "https://developer.nvidia.com/cutensor")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://developer.download.nvidia.com/\
+compute/cutensor/redist/libcutensor/linux-x86_64/libcutensor-linux-x86_64-"
+                                  version "-archive.tar.xz"))
+              (sha256 (base32 "18l6qmfjcn75jsyzlsj66mji8lgab2ih19d0drqavfi2lqna3vgd"))))
+    (build-system copy-build-system)
+    (arguments
+     (list #:substitutable? #f
+           #:strip-binaries? #f
+           #:validate-runpath? #f
+           #:install-plan
+           ''(("include" "include")
+              ("lib" "lib")
+              ("LICENSE" "LICENSE"))))
+    (synopsis "Nvidia cuTENSOR library")
+    (description "This package provides the proprietary cuTENSOR
+library for NVIDIA GPUs.")
+    (license
+     (nonfree:nonfree "https://docs.nvidia.com/cuda/cutensor/latest/license.html"))))
